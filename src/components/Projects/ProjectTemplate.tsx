9051cbd4ffc78114c88381753dff2c5a8ef24ff0
Nick Cunningham <nick.cunningham@leadcrunch.ai>
2023-04-30 15:26:47 -0700

    set up project template

---
+++ src/components/Projects/ProjectTemplate.tsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from "@mui/material";
+
+const ProjectTemplate = ({
+  title,
+  description,
+  image,
+}: {
+  title: string;
+  description: string;
+  image: JSX.Element;
+}) => {
+  return (
+    <Box>
+      <Typography variant="h5">{title}</Typography>
+      <Box>{image}</Box>
+      <Typography variant="body1">{description}</Typography>
+    </Box>
+  );
+};
+
+export default ProjectTemplate;
