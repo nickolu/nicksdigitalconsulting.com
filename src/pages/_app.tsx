e360fb80677bc9993090cb0f6b5e1c27015c26f8
Nick Cunningham <nick.cunningham@leadcrunch.ai>
2023-05-01 16:50:07 -0700

    add ga script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,27 @@ import theme from '@/styles/theme';
 import {ThemeProvider} from '@mui/material';
 
 import type {AppProps} from 'next/app';
+import Script from 'next/script';
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <div>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RRYJQ69KJL');
+        `}
+        </Script>
+      </div>
     </ThemeProvider>
   );
 }
